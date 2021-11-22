883bf6a796a7b3d40cc91c1185b9fef13551d5a4
Ilham_Nugraha <80625504+M-Ilham-Nugraha-Syahputra@users.noreply.github.com>
2021-11-22 23:25:25 +0700

    Add files via upload

---
+++ RetrofitVolley/koneksi/retrofit/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$db   = "workshop_mobile";
+
+$conn = mysqli_connect($host, $user, $password, $db) or die("Gagal menghubungi server!");;
+
+?>
---
+++ RetrofitVolley/koneksi/workshop_mobile.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 22 Nov 2021 pada 17.00
+-- Versi server: 10.4.19-MariaDB
+-- Versi PHP: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `workshop_mobile`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `akun`
+--
+
+CREATE TABLE `akun` (
+  `id` int(3) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `no_telp` varchar(13) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `akun`
+--
+
+INSERT INTO `akun` (`id`, `nama`, `no_telp`, `email`, `alamat`, `password`) VALUES
+(1, 'Ilham Nugraha ', '081249435711', 'ilham@gmail.com', 'Jember- Jatim', '233343'),
+(9, 'asep', '081245462323', 'asep@gmail.com', 'sukowono', '12345');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `akun`
+--
+ALTER TABLE `akun`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `akun`
+--
+ALTER TABLE `akun`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
